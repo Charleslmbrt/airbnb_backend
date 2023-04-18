f8bcb135f4049c9a8da599e1ea3c18425deddaa8
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-04-18 18:29:21 +0200

    update search filters

--- routes/room.js
+++ routes/room.js
@@ -142,51 +142,69 @@ router.post(
 // Get rooms
 router.get("/rooms", async (req, res) => {
   try {
-    const createFilters = (req) => {
-      const filters = {};
+    const filters = {};
 
-      if (req.query.title) {
-        filters.title = new RegExp(req.query.title, "i");
-      }
+    if (req.query.address) {
+      filters.address = new RegExp(req.query.address, "i");
+    }
 
-      if (req.query.priceMin) {
+    if (req.query.type) {
+      filters.type = new RegExp(req.query.type, "i");
+    }
+
+    if (req.query.priceMin) {
+      filters.price = {};
+      filters.price.$gte = req.query.priceMin;
+    }
+
+    if (req.query.priceMax) {
+      if (filters.price) {
+        filters.price.$lte = req.query.priceMax;
+      } else {
         filters.price = {};
-        filters.price.$gte = req.query.priceMin;
+        filters.price.$lte = req.query.priceMax;
       }
+    }
 
-      if (req.query.priceMax) {
-        if (filters.price) {
-          filters.price.$lte = req.query.priceMax;
-        } else {
-          filters.price = {};
-          filters.price.$lte = req.query.priceMax;
-        }
-      }
-      return filters;
-    };
+    // Création d'un objet sort qui servira à gérer le tri
+    let sort = {};
+    // Si on reçoit un query sort === "price-desc"
+    if (req.query.sort === "price-desc") {
+      // On réassigne cette valeur à sort
+      sort = { product_price: -1 };
+    } else if (req.query.sort === "price-asc") {
+      // Si la valeur du query est "price-asc" on réassigne cette autre valeur
+      sort = { product_price: 1 };
+    }
+    // Création de la variable page qui vaut, pour l'instant, undefined
+    let page;
+    // Si le query page n'est pas un nombre >= à 1
+    if (Number(req.query.page) < 1) {
+      // page sera par défaut à 1
+      page = 1;
+    } else {
+      // Sinon page sera égal au query reçu
+      page = Number(req.query.page);
+    }
+    // La variable limit sera égale au query limit reçu
+    let limit = Number(req.query.limit);
+    // On va chercher les offres correspondant aux query de filtre reçus grâce à filters, sort et limit. On populate la clef owner en n'affichant que sa clef account
+    const rooms = await Room.find(filters)
+      .populate({
+        path: "owner",
+        select: "account",
+      })
+      .sort(sort)
+      .skip((page - 1) * limit) // ignorer les x résultats
+      .limit(limit); // renvoyer y résultats
 
-    const filters = createFilters(req);
+    // cette ligne va nous retourner le nombre d'annonces trouvées en fonction des filtres
+    const count = await Room.countDocuments(filters);
 
-    const search = Room.find(filters, { description: false }).populate({
-      path: "user",
-      select: "account",
+    res.json({
+      count: count,
+      offers: rooms,
     });
-
-    if (req.query.sort === "price-asc") {
-      search.sort({ price: 1 });
-    } else if (req.query.sort === "price-desc") {
-      search.sort({ price: -1 });
-    }
-
-    if (req.query.page) {
-      const page = Number(req.query.page);
-      const limit = Number(req.query.limit);
-      search.limit(limit).skip(limit * (page - 1));
-    }
-
-    const rooms = await search;
-
-    res.json(success(rooms));
   } catch (error) {
     res.status(400).json(err(error.message));
   }
